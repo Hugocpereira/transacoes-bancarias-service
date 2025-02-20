3d6f3ec4730edbf4817f5f75eba6ae44fb392a33
Hugocpereira <hugocp100@hotmail.com>
2025-02-20 19:03:36 -0300

    corrigindo bug

--- demo/src/main/java/com/example/demo/lambda/TransacaoLambda.java
+++ demo/src/main/java/com/example/demo/lambda/TransacaoLambda.java
@@ -1,4 +1,73 @@
 package com.example.demo.lambda;
 
-public class TransacaoLambda {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.example.demo.model.Cliente;
+import com.example.demo.model.Transacao;
+import com.example.demo.repository.ClienteRepository;
+import com.example.demo.repository.TransacaoRepository;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.Message;
+import java.util.List;
+import java.util.Map;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+@Component
+public class TransacaoLambda implements RequestHandler<Map<String, Object>, String>{
+    private final ClienteRepository clienteRepository;
+    private final TransacaoRepository transacaoRepository;
+    private final SqsClient sqsClient;
+    private final String queueUrl = "https://sqs.us-east-1.amazonaws.com/123456789012/TransacoesFila.fifo";
+
+    public TransacaoLambda(ClienteRepository clienteRepository, TransacaoRepository transacaoRepository, SqsClient sqsClient) {
+        this.clienteRepository = clienteRepository;
+        this.transacaoRepository = transacaoRepository;
+        this.sqsClient = sqsClient;
+    }
+    private List<Message> receberMensagens(){
+        return sqsClient.receiveMessage(req ->
+                req.queueUrl(queueUrl).
+                        maxNumberOfMessages(10).
+                        waitTimeSeconds(5)
+        ).messages();
+    }
+
+    private void removerMensagemDaFila(String receiptHandle){
+        sqsClient.deleteMessage(req ->
+                req.queueUrl(queueUrl).
+                        receiptHandle(receiptHandle)
+        );
+    }
+    private Map<String, Object> parseJson(String json){
+        return new Gson().fromJson(json, new TypeToken<Map<String, Object>>(){}.getType());
+    }
+
+    @Override
+    public String handleRequest(Map<String, Object> input, Context context){
+        List<Message> messages = receberMensagens();
+        for(Message message : messages){
+            Map<String, Object> body = parseJson(message.body());
+            String clienteId = (String) body.get("cliente_id");
+            double valor = (double) body.get("valor");
+
+            processarTransacao(clienteId, valor);
+            removerMensagemDaFila(message.receiptHandle());
+        }
+        return "Processamento concluido";
+
+    }
+
+    private void processarTransacao(String clienteId, double valor){
+        Cliente cliente = clienteRepository.findById(clienteId).orElseThrow();
+        cliente.setSaldo(cliente.getSaldo() + valor);
+
+        Transacao transacao = new Transacao();
+        transacao.setClienteId(clienteId);
+        transacao.setValor(valor);
+
+        transacaoRepository.save(transacao);
+        clienteRepository.save(cliente);
+    }
 }
--- demo/src/main/java/com/example/demo/service/TransacaoService.java
+++ demo/src/main/java/com/example/demo/service/TransacaoService.java
@@ -1,4 +1,40 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Cliente;
+import com.example.demo.model.Transacao;
+import com.example.demo.repository.TransacaoRepository;
+import com.example.demo.repository.ClienteRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Optional;
+
+@Service
 public class TransacaoService {
+    private final ClienteRepository clienteRepository;
+    private final TransacaoRepository transacaoRepository;
+
+    public TransacaoService(ClienteRepository clienteRepository, TransacaoRepository transacaoRepository) {
+        this.clienteRepository = clienteRepository;
+        this.transacaoRepository = transacaoRepository;
+    }
+
+    @Transactional
+    public void processarTransacao(String clienteId, double valor){
+        Optional<Cliente> clienteOptional = clienteRepository.findById(clienteId);
+        if(clienteOptional.isEmpty()){
+            throw new RuntimeException("Cliente não encontrado.");
+        }
+
+        Cliente cliente = clienteOptional.get();
+        double novoSaldo = cliente.getSaldo() + valor;
+        cliente.setSaldo(novoSaldo);
+
+
+        Transacao transacao = new Transacao();
+        transacao.setClienteId(clienteId);
+        transacao.setValor(valor);
+
+        transacaoRepository.save(transacao);
+        clienteRepository.save(cliente);
+     }
 }
